daa9746c8fd58edcb5f0555a0d442d9bf0c63443
Shahi77 <shahiinfinity02@gmail.com>
2023-02-15 19:32:49 +0530

    added buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+let myLead=[]
+let inputEl = []
+
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked from addEventListener")
+})
